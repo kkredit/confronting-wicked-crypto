9be1dfdef06332b1fdc476d93ef3385c6d824aea
Kevin Kredit <k.kredit.us@ieee.org>
2019-12-03 17:07:25 -0500

    Add more references, bib conversion script

---
+++ convert-to-bibliography.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+INFILE="Annotated-bibliography.md"
+OUTFILE="Bibliography.bib"
+PRINTLN="false"
+
+echo "% This is an auto-generated bibliography file based on $INFILE" > $OUTFILE
+echo "" >> $OUTFILE
+
+while read LINE; do
+
+  if [[ "$LINE" =~ '```' ]]; then
+    echo "" >> $OUTFILE
+    PRINTLN="false"
+  fi
+
+  if [[ "$PRINTLN" == "true" ]]; then
+    echo $LINE >> $OUTFILE
+  fi
+
+  if [[ "$LINE" =~ '```bib' ]]; then
+    PRINTLN="true"
+  fi
+
+done < $INFILE
