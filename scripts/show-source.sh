c1d7847b7b61b6555e845935f2afe962eb8c7476
Kevin Kredit <k.kredit.us@ieee.org>
2020-02-05 18:11:20 -0500

    Scripts: beef up show-source.sh

--- scripts/show-source.sh
+++ scripts/show-source.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-FILE=$1
-NAME="$(cat $FILE | head -1 | sed 's/# //')"
-AUTHOR="$(cat $FILE | grep -A1 "\sauthor =" | tr '\n' ' ' | cut -d{ -f2 | cut -d} -f1)"
-URL=$(cat $FILE | grep -A1 "\surl =" | tr '\n' ' ' | cut -d{ -f2 | cut -d} -f1)
+# set -x
+pushd $(git rev-parse --show-toplevel)/reading >/dev/null
+function cleanup() {
+    popd > /dev/null
+    set +x
+}
+trap cleanup EXIT
+
+if [[ $# == 1 && -f $1 ]]; then
+    FILE=$1
+else
+    FILE="$(grep -riIsc "$@" . | sort -t: -n -k2 | tail -1 | cut -d: -f1)"
+    FILE="$(readlink -f "$FILE")"
+fi
+
+if [[ ! -f "$FILE" ]]; then
+    echo "Sorry, file \"$FILE\" not found."
+    exit 1
+fi
+
+NAME="$(cat "$FILE" | head -1 | sed 's/# //')"
+AUTHOR="$(cat "$FILE" | grep -A1 "\sauthor =" | tr '\n' ' ' | cut -d{ -f2 | cut -d} -f1)"
+REF="$(cat "$FILE" | grep -A1 "\`bib" | tr '\n' ' ' | cut -d{ -f2 | cut -d, -f1)"
+URL=$(cat "$FILE" | grep -A1 "\surl =" | tr '\n' ' ' | cut -d{ -f2 | cut -d} -f1)
+
+YELLOW='\e[0;33m'
+LIGHT_GREEN='\e[1;32m'
+LIGHT_CYAN='\e[1;36m'
+BLUE='\e[0;34m'
+NO_COLOR='\e[0m'
 
 echo
-echo "\"$NAME\""
+echo -e "$LIGHT_CYAN$NAME$NO_COLOR"
+echo -e "by $YELLOW$AUTHOR$NO_COLOR"
 echo
-echo "by $AUTHOR"
+echo -e "$BLUE$URL$NO_COLOR"
+echo -e "$FILE"
 echo
-echo "  $URL"
+echo -e "cite as $LIGHT_GREEN$REF$NO_COLOR"
 echo
