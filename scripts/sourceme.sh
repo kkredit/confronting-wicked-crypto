3af80338acb75ec18ec14ff672e7c48e321cd05c
Kevin Kredit <k.kredit.us@ieee.org>
2020-02-20 10:02:03 -0500

    Scripts: export variables in sourceme.sh

--- scripts/sourceme.sh
+++ scripts/sourceme.sh
@@ -1,7 +1,7 @@
 # Source this file
 
-READING_DIR="$(git rev-parse --show-toplevel)/reading"
-SCRIPTS_DIR="$(git rev-parse --show-toplevel)/scripts"
+export READING_DIR="$(git rev-parse --show-toplevel)/reading"
+export SCRIPTS_DIR="$(git rev-parse --show-toplevel)/scripts"
 
 alias new="$SCRIPTS_DIR/new-source.sh"
 alias show="$SCRIPTS_DIR/show-source.sh"
