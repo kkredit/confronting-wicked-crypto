991345b152baa9c1922e3a285ad24a56f3378743
Kevin Kredit <k.kredit.us@ieee.org>
2020-07-24 12:05:16 -0400

    Add 'notes' alias to preview reading notes

--- scripts/sourceme.sh
+++ scripts/sourceme.sh
@@ -22,6 +22,8 @@ alias gen="$SCRIPTS_DIR/bib-gen.sh"
 alias cite="$SCRIPTS_DIR/count-citations.sh"
 alias todo="(cd $BASE_DIR && grep -nE '(TODO|FIXME)' *.tex)"
 alias fixme="(cd $BASE_DIR && grep -nE 'FIXME' *.tex)"
+alias notes="(cd $BASE_DIR/reading && fzf --layout=reverse --info=inline --border --preview-window=left:120:wrap \
+    --preview='$SCRIPTS_DIR/show-source.sh {} && printf \"====================\n\n\" && bat --plain --color=always {}')"
 
 alias pdf="xdg-open build/Thesis.pdf"
 alias gvsu="xdg-open build/Thesis-GVSU.pdf"
