968875cef3be2e4011d853f705ab99113d94925a
Kevin Kredit <k.kredit.us@ieee.org>
2020-07-24 15:23:43 -0400

    Change syntax for argdown citations and update 'cite' script

--- scripts/argdown-insert-citations.sh
+++ scripts/argdown-insert-citations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -eEuox pipefail
+set -eEuo pipefail
 
 source $(git rev-parse --show-toplevel)/scripts/sourceme.sh
 cd $BASE_DIR
@@ -10,27 +10,23 @@ function get_ref_from_citation {
 }
 
 function get_full_citations_in_file {
-  grep -o "{cite: [a-zA-Z_0-9]*}" $1 | uniq | sed 's/ /+/'
-}
-
-function get_stripped_citation {
-  echo $1 | cut -d'+' -f2 | cut -d'}' -f1
+  grep -o "#cite_[a-zA-Z_0-9]*" $1 | uniq
 }
 
 function parse_infile_to_outfile {
   # echo "IN: $1; OUT: $2"
   cp $1 $2
   for CITE in $(get_full_citations_in_file $1); do
-    local PLAIN_CITE="$(get_stripped_citation $CITE)"
-    local ACTUAL_CITE=${CITE/+/ }
+    local PLAIN_CITE=${CITE/\#cite_/}
+    # local ACTUAL_CITE=${CITE/+/ }
     local REF="$(get_ref_from_citation $PLAIN_CITE)"
-    [[ "" == "$REF" ]] && exitprint 1 "Citation for $ACTUAL_CITE not found"
+    [[ "" == "$REF" ]] && exitprint 1 "Citation for $CITE ($PLAIN_CITE) not found"
     # echo "CITE: $PLAIN_CITE; REF: $REF"
-    sed -i "s/$ACTUAL_CITE/\\\\[$REF\\\\]/g" $2
+    sed -i "s/$CITE/\\\\[$REF\\\\]/g" $2
   done
 }
 
 mkdir -p arguments/w_citations
 for ARG in $(cd arguments; ls *.argdown); do
-  parse_infile_to_outfile arguments/$ARG arguments/citations/$ARG
+  parse_infile_to_outfile arguments/$ARG arguments/w_citations/$ARG
 done
--- scripts/count-citations.sh
+++ scripts/count-citations.sh
@@ -4,13 +4,19 @@ cd $(git rev-parse --show-toplevel)
 source ./scripts/sourceme.sh
 # set -x
 
-function print_number() {
+function print_number_in_tex() {
     echo $(grep "{$1}" *.tex | wc -l)
 }
 
+function print_number_in_argdown() {
+    echo $(grep "cite_$1" arguments/*.argdown | wc -l)
+}
+
 function print_count() {
-    COUNT=$(print_number $1)
-    echo -e "$LIGHT_CYAN$1$NO_COLOR is cited $COUNT time$(((1 == COUNT)) || echo s)."
+    COUNT_TEX=$(print_number_in_tex $1)
+    COUNT_ARGDOWN=$(print_number_in_argdown $1)
+    printf "$LIGHT_CYAN$1$NO_COLOR is cited $LIGHT_GREEN$COUNT_TEX$NO_COLOR time$(((1 == COUNT_TEX)) || echo s) in writing"
+    printf " and $LIGHT_GREEN$COUNT_ARGDOWN$NO_COLOR time$(((1 == COUNT_ARGDOWN)) || echo s) in arguments.\n"
 }
 
 if (( 2 <= $# )) && [[ "-c" == "$1" ]]; then
@@ -18,9 +24,10 @@ if (( 2 <= $# )) && [[ "-c" == "$1" ]]; then
     print_count $2
 elif (( 2 <= $# )) && [[ "-n" == "$1" ]]; then
     # Print number only
-    print_number $2
+    echo $(( $(print_number_in_tex $2) + $(print_number_in_argdown $2) ))
 else
     print_count $1
     echo
-    grep --color=auto -n "\\cite{$1}" *.tex
+    grep --color=auto -n "\\\\cite{$1}" *.tex
+    grep --color=auto -n "#cite_$1" arguments/*.argdown
 fi
