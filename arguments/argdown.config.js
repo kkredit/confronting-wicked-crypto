6e8b6d7a9cad9967977f7802b3523a20c27109a8
Kevin Kredit <k.kredit.us@ieee.org>
2020-10-28 20:49:59 -0400

    Begin arguments chapter

--- arguments/argdown.config.js
+++ arguments/argdown.config.js
@@ -1,63 +1,89 @@
 // See https://argdown.org/guide/configuration-cheatsheet.html
 
 module.exports = {
-  author: 'Kevin Kredit',
-  inputPath: './build/w_citations/comprehensive.ad',
+  author: "Kevin Kredit",
+  inputPath: "./build/w_citations/comprehensive.ad",
   model: {
-    mode: 'strict',
-    removeTagsFromText: true
+    mode: "strict",
+    removeTagsFromText: true,
   },
   svg: {
-    outputDir: './build'
+    outputDir: "./build",
   },
   processes: {
     file: {
       inputPath: process.argv[process.argv.length - 1],
-      process: 'export-svg'
+      process: "export-svg",
     },
     whole: {
-      process: 'export-svg',
-      outputSuffix: ".whole"
+      process: "export-svg",
+      outputSuffix: ".whole",
+    },
+    contrib: {
+      process: "export-svg",
+      outputSuffix: ".contrib",
+      dot: {
+        graphVizSettings: {
+          // rankdir: "LR"
+        },
+      },
+      group: {
+        groupDepth: 3,
+      },
+      selection: {
+        selectedSections: [
+          "Central Issues",
+          "Response Measures",
+          "Arguments for Measures",
+          "Exceptional Access",
+        ],
+        // selectedTags: ["core", "ea-itself"],
+        // selectElementsWithoutTag: true,
+      },
     },
     ea_proposals: {
-      process: 'export-svg',
+      process: "export-svg",
       outputSuffix: ".ea_proposals",
       selection: {
-        selectedTags: ['ea-proposal', 'neutral-ea'],
-        selectElementsWithoutTag: true
-      }
+        selectedTags: ["ea-proposal", "neutral-ea"],
+        selectElementsWithoutTag: true,
+      },
     },
     pro: {
-      process: 'export-svg',
+      process: "export-svg",
       outputSuffix: ".pro",
       selection: {
-        selectedTags: ['ea-itself', 'pro-ea'],
+        selectedTags: ["ea-itself", "pro-ea"],
         selectElementsWithoutTag: true,
-      }
+      },
     },
     anti: {
-      process: 'export-svg',
+      process: "export-svg",
       outputSuffix: ".anti",
       selection: {
-        selectedTags: ['ea-itself', 'anti-ea'],
+        selectedTags: ["ea-itself", "anti-ea"],
         selectElementsWithoutTag: true,
-      }
+      },
     },
     fallacies: {
       dot: {
         graphVizSettings: {
-          rankdir: 'LR'
-        }
+          rankdir: "LR",
+        },
       },
-      process: 'export-svg',
+      process: "export-svg",
       outputSuffix: ".fallacies",
       selection: {
         selectedSections: [
-          'Central Issues', 'Response Measures', 'Exceptional Access', 'Fallacies', 'Fallacious Arguments'
+          "Central Issues",
+          "Response Measures",
+          "Exceptional Access",
+          "Fallacies",
+          "Fallacious Arguments",
         ],
-        selectedTags: ['core', 'ea-itself', 'fallacy', 'fallacious'],
-        selectElementsWithoutTag: true
-      }
-    }
-  }
+        selectedTags: ["core", "ea-itself", "fallacy", "fallacious"],
+        selectElementsWithoutTag: true,
+      },
+    },
+  },
 };
