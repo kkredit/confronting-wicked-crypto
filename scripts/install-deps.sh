a0a6cbce90fd2eef422fcd944fdbc47453c6c554
Andrew Kalafut <kalafuta@KALAFUTA-EE2640.office.ads.gvsu.edu>
2021-02-23 13:31:15 -0500

    Removed packages that do not exist in ubuntu 20. Added missing dependencies. Adjusted to work on wsl

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -13,20 +13,20 @@ sudo apt-get install -y \
     texlive-latex-extra \
     texlive-science \
     texlive-xetex \
-    texlive-generic-extra \
+    texlive-plain-generic \
     texlive-publishers \
     latexmk \
     inkscape \
     libcanberra-gtk-module \
     libcanberra-gtk3-module \
-    npm
+    inkscape
 
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 sudo apt-get install -y --no-install-recommends \
     nodejs
 sudo npm install -g @argdown/cli
 
-if which snap &>/dev/null; then
+if which snap &>/dev/null && !grep microsoft /proc/version &>/dev/null; then
     snap install drawio
 else
     URL=$(curl -sL https://github.com/jgraph/drawio-desktop/releases/latest | grep "\.deb" | head -1  | cut -d\" -f2)
